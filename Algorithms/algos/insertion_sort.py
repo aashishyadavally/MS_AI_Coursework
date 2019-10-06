38158bd3cd1cd08f383ecd20b9643f82765aaa99
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-10-06 14:03:09 -0400

    Added reverse for insertion sort algorithm

--- Algorithms/algos/insertion_sort.py
+++ Algorithms/algos/insertion_sort.py
@@ -6,7 +6,32 @@ Author
 Aashish Yadavally
 """
 
-def Insertion_Sort(array: list) -> list:
+def get_reverse_condition(a, b, reverse):
+    """
+    Get condition for while-loop
+
+    Arguments
+    ---------
+        a (float):
+            While-condition argument
+        b (float):
+            While-condition argument
+        reverse (bool):
+            True, indicates sorting in non-increasing order
+            False, indicates sorting in non-increading order
+
+    Returns
+    -------
+        (bool):
+            a < b if reverse is True, else a > b        
+    """
+    if reverse == True:
+        return a < b
+    else:
+        return a > b
+
+
+def Insertion_Sort(array: list, reverse: bool=False) -> list:
     """
     Implementation for the insertion sort algorithm
 
@@ -14,6 +39,9 @@ def Insertion_Sort(array: list) -> list:
     ---------
         array (list):
             Array to be sorted
+        reverse (bool):
+            True, indicates sorting in non-increasing order
+            False, indicates sorting in non-increading order
 
     Returns
     -------
@@ -24,10 +52,10 @@ def Insertion_Sort(array: list) -> list:
         # Current comparison element
         key = array[j]
         i = j - 1
-        while i >= 0 and array[i] > key:
+
+        while i >= 0 and get_reverse_condition(array[i], key, reverse):
             # Movement or shift to right
             array[i + 1] = array[i]
             i = i - 1
         array[i + 1] = key
     return array
-
